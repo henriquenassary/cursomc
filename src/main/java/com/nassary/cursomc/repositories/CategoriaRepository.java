dbff4a10f402d69c399a0258bfe27101b62456c2
Henrique Nassary <henriquenassary@gmail.com>
2020-12-21 16:30:12 -0300

    Inserindo Categorias no banco H2

---
+++ src/main/java/com/nassary/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.nassary.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nassary.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+	
+}
